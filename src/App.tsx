d412f7a7b596d42aa2bebf0a004882faf576de84
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-12-16 00:55:53 +0200

    Added some ambient light

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 // import { useState } from 'react'
 import 'src/App.css';
 import { Sky } from '@react-three/drei';
+import { Ground } from './components/Ground';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <Sky sunPosition={[100, 20, 100]} />
+    <>
+      <Sky sunPosition={[100, 20, 100]} />
+      <ambientLight intensity={1.5} />
+      <Ground />
+    </>
   );
 }
 
