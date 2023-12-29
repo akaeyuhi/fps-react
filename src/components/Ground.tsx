332bbc0ef832b19003d4a669158cd8d1d41436c4
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-12-29 19:09:00 +0200

    loaded texture in ground component

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,8 +1,16 @@
+import * as THREE from 'three';
+import { useTexture } from '@react-three/drei';
+import floorTexture from './assets/floor.png';
+
 export function Ground() {
+  const texture = useTexture(floorTexture);
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+
   return (
     <mesh position={[0, -5, 0]} rotation-x={-Math.PI / 2}>
       <planeGeometry args={[500, 500]} />
-      <meshStandardMaterial color="gray" />
+      <meshStandardMaterial color="gray" map={texture} map-repeat={[360, 360]} />
     </mesh>
   );
 }
