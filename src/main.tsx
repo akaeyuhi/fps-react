ae00404205914719203f21eb812501b91616c085
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-12-29 19:17:36 +0200

    path fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Canvas } from '@react-three/fiber';
 import App from './App.tsx';
-import 'index.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
