0d89b3cd02cee3f46c6c60c4818a9672518c39cd
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-12-29 19:58:21 +0200

    added basic player movement

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,8 +1,35 @@
-import { RigidBody } from '@react-three/rapier';
+import * as THREE from 'three';
+import { RigidBody, RigidBodyTypeString } from '@react-three/rapier';
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { usePersonControls } from '../utils/hooks/usePersonControls.ts';
+
+const MOVE_SPEED = 5;
+const direction = new THREE.Vector3();
+const frontVector = new THREE.Vector3();
+const sideVector = new THREE.Vector3();
 
 export function Player() {
+  const playerRef = useRef<any>();
+  const {
+    forward, backward, left, right, jump,
+  } = usePersonControls();
+
+  useFrame(state => {
+    if (!playerRef.current) return;
+
+    const velocity = playerRef.current.linvel();
+
+    frontVector.set(0, 0, +backward - +forward);
+    sideVector.set(+left - +right, 0, 0);
+    direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(MOVE_SPEED);
+
+    playerRef.current.wakeUp();
+    playerRef.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z });
+  });
+
   return (
-    <RigidBody position={[0, 1, -2]}>
+    <RigidBody position={[0, 1, -2]} ref={playerRef}>
       <mesh>
         <capsuleGeometry args={[0.5, 0.5]} />
       </mesh>
