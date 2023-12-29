17dc9bd24943c5e56bc97f92056e9dd8fcb97133
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-12-29 19:17:47 +0200

    make floor rigid

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { useTexture } from '@react-three/drei';
-import floorTexture from './assets/floor.png';
+import { RigidBody } from '@react-three/rapier';
+import floorTexture from '../assets/textures/floor.png';
 
 export function Ground() {
   const texture = useTexture(floorTexture);
@@ -8,9 +9,11 @@ export function Ground() {
   texture.wrapT = THREE.RepeatWrapping;
 
   return (
-    <mesh position={[0, -5, 0]} rotation-x={-Math.PI / 2}>
-      <planeGeometry args={[500, 500]} />
-      <meshStandardMaterial color="gray" map={texture} map-repeat={[100, 100]} />
-    </mesh>
+    <RigidBody>
+      <mesh position={[0, -5, 0]} rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[500, 500]} />
+        <meshStandardMaterial color="gray" map={texture} map-repeat={[100, 100]} />
+      </mesh>
+    </RigidBody>
   );
 }
