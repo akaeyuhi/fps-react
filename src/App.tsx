0f06bdd7ff1bcd0b1b4664c3478814d1812c5a5a
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-12-29 19:28:47 +0200

    added player component to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { PointerLockControls, Sky } from '@react-three/drei';
 import { Physics, RigidBody } from '@react-three/rapier';
 import { Ground } from './components/Ground';
+import { Player } from './components/Player.tsx';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
       <ambientLight intensity={1.5} />
       <Physics gravity={[0, -20, 0]}>
         <Ground />
+        <Player />
         <RigidBody>
           <mesh position={[0, 3, -5]}>
             <boxGeometry />
