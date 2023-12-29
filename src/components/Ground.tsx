c27b06e0e6ee9e97fe559fea4637b255f5f9b750
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-12-29 20:16:45 +0200

    added cuboid collider

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { useTexture } from '@react-three/drei';
-import { RigidBody } from '@react-three/rapier';
+import { CuboidCollider, RigidBody } from '@react-three/rapier';
 import floorTexture from '../assets/textures/floor.png';
 
 export function Ground() {
@@ -9,11 +9,12 @@ export function Ground() {
   texture.wrapT = THREE.RepeatWrapping;
 
   return (
-    <RigidBody>
-      <mesh position={[0, -5, 0]} rotation-x={-Math.PI / 2}>
+    <RigidBody type="fixed" colliders={false}>
+      <mesh position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
         <planeGeometry args={[500, 500]} />
         <meshStandardMaterial color="gray" map={texture} map-repeat={[100, 100]} />
       </mesh>
+      <CuboidCollider args={[500, 2, 500]} position={[0, -2, 0]} />
     </RigidBody>
   );
 }
