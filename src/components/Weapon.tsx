5e36f393be204df6c32e6b01002dd497efccaef6
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-12-30 22:52:11 +0200

    added shoot animation

--- src/components/Weapon.tsx
+++ src/components/Weapon.tsx
@@ -1,10 +1,11 @@
 import * as THREE from 'three';
 import * as TWEEN from '@tweenjs/tween.js';
 import { useEffect, useRef, useState } from 'react';
-import { useFrame } from '@react-three/fiber';
+import { useFrame, useLoader } from '@react-three/fiber';
 import { WeaponModel } from '../models/WeaponModel.jsx';
 import { usePointerLockControlsStore } from '../store/LockContolsStore';
 import { useAimingStore } from '../store/AimingStore';
+import FlashShoot from '../assets/images/shoot.png';
 
 const SHOOT_BUTTON = parseInt(import.meta.env.VITE_SHOOT_BUTTON, 10);
 const AIM_BUTTON = parseInt(import.meta.env.VITE_AIM_BUTTON, 10);
@@ -18,17 +19,20 @@ export function Weapon(props) {
   const [isShooting, setIsShooting] = useState(false);
   const setIsAiming = useAimingStore(state => state.setIsAiming);
   const weaponRef = useRef<any>();
+  const texture = useLoader(THREE.TextureLoader, FlashShoot);
+
+  const [flashAnimation, setFlashAnimation] = useState(null);
 
   const mouseButtonHandler = (button: number, state: boolean) => {
     if (!usePointerLockControlsStore.getState().isLock) return;
 
     switch (button) {
-    case SHOOT_BUTTON:
-      setIsShooting(state);
-      break;
-    case AIM_BUTTON:
-      setIsAiming(state);
-      break;
+      case SHOOT_BUTTON:
+        setIsShooting(state);
+        break;
+      case AIM_BUTTON:
+        setIsAiming(state);
+        break;
     }
   };
 
@@ -76,8 +80,33 @@ export function Weapon(props) {
     setRecoilAnimation(twRecoilAnimation);
   };
 
+  const [flashOpacity, setFlashOpacity] = useState(0);
+
+  const initFlashAnimation = () => {
+    const currentFlashParams = { opacity: 0 };
+
+    const twFlashAnimation = new TWEEN.Tween(currentFlashParams)
+      .to({ opacity: 1 }, recoilDuration)
+      .easing(easing)
+      .onUpdate(() => {
+        setFlashOpacity(() => currentFlashParams.opacity);
+      })
+      .onComplete(() => {
+        setFlashOpacity(() => 0);
+      });
+
+    setFlashAnimation(twFlashAnimation);
+  }
+
+  useEffect(() => {
+    initFlashAnimation();
+  }, []);
+
   const startShooting = () => {
+    if (!recoilAnimation) return;
+
     (recoilAnimation as any).start();
+    (flashAnimation as any).start();
   };
 
   useEffect(() => {
@@ -95,7 +124,12 @@ export function Weapon(props) {
     // eslint-disable-next-line react/jsx-props-no-spreading
     <group {...props}>
       <group ref={weaponRef}>
-        <WeaponModel />
+        <mesh position={[0, 0.05, -2]} scale={[1, 1, 0]}>
+          <planeGeometry attach="geometry" args={[1, 1]}/>
+          <meshBasicMaterial attach="material" map={texture} transparent={true}
+                             opacity={flashOpacity}/>
+        </mesh>
+        <WeaponModel/>
       </group>
     </group>
   );
