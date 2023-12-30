bc56d38619d3a1c987cfffb8701b36d97eb744f3
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-12-30 21:30:05 +0200

    added recoil and moving animation

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,17 +1,27 @@
 import * as THREE from 'three';
 import * as RAPIER from '@dimforge/rapier3d-compat';
-import { RigidBody, useRapier } from '@react-three/rapier';
-import { useRef } from 'react';
+import * as TWEEN from '@tweenjs/tween.js';
+import { CapsuleCollider, RigidBody, useRapier } from '@react-three/rapier';
+import { useEffect, useRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
+import { Weapon } from './Weapon.tsx';
 import { usePersonControls } from '../utils/hooks/usePersonControls.ts';
 
 const MOVE_SPEED = 5;
 const direction = new THREE.Vector3();
 const frontVector = new THREE.Vector3();
 const sideVector = new THREE.Vector3();
+const rotation = new THREE.Vector3();
 
 export function Player() {
   const playerRef = useRef<any>();
+  const objectInHandRef = useRef<any>();
+
+  const swayingObjectRef = useRef<any>();
+  const [swayingAnimation, setSwayingAnimation] = useState(null);
+  const [swayingBackAnimation, setSwayingBackAnimation] = useState(null);
+  const [isSwayingAnimationFinished, setIsSwayingAnimationFinished] = useState(true);
+
   const rapier = useRapier();
   const {
     forward, backward, left, right, jump,
@@ -19,6 +29,36 @@ export function Player() {
 
   const doJump = () => {
     playerRef.current.setLinvel({ x: 0, y: 8, z: 0 });
+  };
+
+  const initSwayingObjectAnimation = () => {
+    const currentPosition = new THREE.Vector3(0, 0, 0);
+    const initialPosition = new THREE.Vector3(0, 0, 0);
+    const newPosition = new THREE.Vector3(-0.05, 0, 0);
+    const animationDuration = 300;
+    const easing = TWEEN.Easing.Quadratic.Out;
+
+    const twSwayingAnimation = new TWEEN.Tween(currentPosition)
+      .to(newPosition, animationDuration)
+      .easing(easing)
+      .onUpdate(() => {
+        swayingObjectRef.current.position.copy(currentPosition);
+      });
+
+    const twSwayingBackAnimation = new TWEEN.Tween(currentPosition)
+      .to(initialPosition, animationDuration)
+      .easing(easing)
+      .onUpdate(() => {
+        swayingObjectRef.current.position.copy(currentPosition);
+      })
+      .onComplete(() => {
+        setIsSwayingAnimationFinished(true);
+      });
+
+    twSwayingAnimation.chain(twSwayingBackAnimation);
+
+    setSwayingAnimation(twSwayingAnimation);
+    setSwayingBackAnimation(twSwayingBackAnimation);
   };
 
   useFrame(state => {
@@ -36,23 +76,47 @@ export function Player() {
     const { world } = rapier;
     const ray = world.castRay(
       new RAPIER.Ray(playerRef.current.translation(), { x: 0, y: -1, z: 0 }),
-      0,
-      true,
+      2,
+      false,
     );
-    const grounded = ray && ray.collider && Math.abs(ray.toi) <= 1.5;
+    const grounded = ray && ray.collider && Math.abs(ray.toi) < 1.25;
 
     if (jump && grounded) doJump();
 
-    // moving camera
     const { x, y, z } = playerRef.current.translation();
     state.camera.position.set(x, y, z);
+
+    objectInHandRef.current.rotation.copy(state.camera.rotation);
+    objectInHandRef.current.position.copy(state.camera.position)
+      .add(state.camera.getWorldDirection(rotation));
+
+    const isMoving = direction.length() > 0;
+
+    if (isMoving && isSwayingAnimationFinished) {
+      setIsSwayingAnimationFinished(false);
+      if (swayingAnimation) { (swayingAnimation as any).start(); }
+    }
+
+    TWEEN.update();
   });
 
+  useEffect(() => {
+    initSwayingObjectAnimation();
+  }, []);
+
   return (
-    <RigidBody position={[0, 1, -2]} ref={playerRef}>
-      <mesh castShadow>
-        <capsuleGeometry args={[0.5, 0.5]} />
-      </mesh>
-    </RigidBody>
+    <>
+      <RigidBody colliders={false} mass={1} ref={playerRef} lockRotations>
+        <mesh castShadow>
+          <capsuleGeometry args={[0.5, 0.5]} />
+          <CapsuleCollider args={[0.75, 0.5]} />
+        </mesh>
+      </RigidBody>
+      <group ref={objectInHandRef}>
+        <group ref={swayingObjectRef}>
+          <Weapon position={[0.2, -0.2, 0.3]} scale={0.12} />
+        </group>
+      </group>
+    </>
   );
 }
